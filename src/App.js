d7d235c3cd28ccff9c629c8cf9a109a97ae59874
SotMDev <m.erkan.ozturk@gmail.com>
2023-01-17 17:09:01 +0300

    Products taken from endpoint.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
-import { Container } from '@mui/material';
+import {Container} from '@mui/material';
 import {Route, Routes} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from 'react-query';
 import ProductList from "./pages/ProductList";
 import ProductDetail from "./pages/ProductDetail";
 import Home from "./pages/Home";
 import './assets/scss/helpers.scss';
 
 function App() {
+	const queryClient = new QueryClient();
 
-  return (
-    <div className="App">
-		<Header />
-		<Container maxWidth={"md"}>
-			<Routes>
-				<Route exact path="/" element={<Home/>}></Route>
-				<Route exact path="/products">
-					<Route index element={<ProductList/>}/>
-					<Route path=":id" element={<ProductDetail/>}/>
-				</Route>
-			</Routes>
-		</Container>
-    </div>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className="App">
+				<Header/>
+				<Container maxWidth={"md"}>
+					<Routes>
+						<Route exact path="/" element={<Home/>}></Route>
+						<Route exact path="/products">
+							<Route index element={<ProductList/>}/>
+							<Route path=":id" element={<ProductDetail/>}/>
+						</Route>
+					</Routes>
+				</Container>
+			</div>
+		</QueryClientProvider>
+	);
 
 }
 
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {useQuery} from 'react-query';
 
 const ProductList = () => {
+
+	const productsEndPoint = "https://fakestoreapi.com/products";
+	const {
+		data,
+		isLoading,
+		isFetching
+	} = useQuery(["products"], () => fetch(productsEndPoint).then(res => res.json()));
+
 	return (
 		<>
-			List
+			<div className="product-filtering">
+				Filtering
+			</div>
+			<div className={"product-container"}>
+				{
+					isLoading || isFetching ? <h1>Loading</h1>
+						:
+						<div>
+							{data.map((product, index) => (
+								<div key={index}>
+									<div>{product.id}</div>
+									<div>{product.title}</div>
+									<img width={100} height={100} src={product.image}></img>
+									<div>{product.price}</div>
+									<div>{product.rating?.rate}</div>
+								</div>
+							))}
+						</div>
+				}
+			</div>
+			<div className="product-per-page">
+				perPage
+			</div>
 		</>
 	)
 }
