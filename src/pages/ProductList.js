4c28f07a2bdc41becc488d16d71af8f9a0f33756
SotMDev <m.erkan.ozturk@gmail.com>
2023-01-19 11:59:01 +0300

    Minor fix

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -32,6 +32,7 @@ const ProductList = () => {
 		isFetching,
 		refetch
 	} = useQuery(["products"], () => fetch(productsEndPoint).then(res => res.json()).catch(err => {
+			console.log(err);
 			setToast({status: true, type: 'error', message: 'Something went wrong!'});
 		})
 		,
